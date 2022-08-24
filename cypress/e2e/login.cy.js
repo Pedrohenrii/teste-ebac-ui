d6681846c256c4808ad13b456b44250164cece26
Pedro Henrique <peehclara@gmail.com>
2022-08-24 10:08:11 -0300

    usando massa de dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types = "cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('funcionalidade login', () =>{
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -17,8 +18,26 @@ cy.get('#password').type('teste@teste.com')
 cy.get('.woocommerce-form > .button').click ()
 
 cy.get('.page-title') .should ('contain','Minha conta')
-cy.get('.woocommerce-MyAccount-content > :nth-child(2)') .should ( 'contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
+cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should ( 'contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
  })
+   
+ it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+    cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click ()
+    cy.get('.page-title') .should ('contain','Minha conta') 
+ });
+  
+  it('Deve fazer login com sucesso usando fixture ', () => {
+    cy.fixture('perfil').then(dados=> {
+        cy.get('#username').type(dados.usuario)
+        cy.get('#password').type(dados.senha, {log: false})
+        cy.get('.woocommerce-form > .button').click ()
+        cy.get('.page-title') .should ('contain','Minha conta') 
+    })
+    
+  });
+
  
  it('deve exibir uma mensagem de texto ao inserir usuario  invalídos ', () =>{cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
  cy.get('#username').type('alunc@teste.com')
---
+++ cypress/e2e/produto.cy.js
@@ -0,0 +1,32 @@
+/// <reference types = "cypress" />
+
+describe('Funcionalidade Página de produto', () => {
+
+    beforeEach(() => {
+        cy.visit('produtos')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+
+    });
+
+    it('Deve adiconar o produto ao carrinho', function () {
+
+            var quantidade = 3;
+            cy.get('[class="product-block grid"]')
+            .contains('Ariel Roll Sleeve Sweatshirt').click();
+            cy.get('.button-variable-item-L').click();
+            cy.get('.button-variable-item-Green').click();
+            cy.get('.input-text').clear().type(quantidade);
+            cy.get('.single_add_to_cart_button').click();
+
+            cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+        });
+
+});
